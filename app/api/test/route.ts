f71c60be3e3955c6bb93ae0942dbed1cb67422e3
Filip Grafek <grafekfik737@gmail.com>
2025-12-17 19:22:47 +0100

    done

---
+++ app/api/test/route.ts
@@ -0,0 +1,8 @@
+import { redis } from "@/lib/redis";
+
+export async function GET() {
+  await redis.set("hello", "world");
+  const value = await redis.get("hello");
+
+  return Response.json({ value });
+}
